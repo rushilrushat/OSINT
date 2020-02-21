0509a1cf04e1eb730ab62da78234f31ea3a4a8cf
rushilrushat <rushilrushat@gmail.com>
2020-02-22 00:36:58 +0530

    PDF Generated but Pdf Formetting is not done

--- app/src/main/java/com/example/testapp/iplookup/iplookup.kt
+++ app/src/main/java/com/example/testapp/iplookup/iplookup.kt
@@ -19,6 +19,9 @@ import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import com.example.testapp.R
 import com.example.testapp.iplookup.data.ipdata
+import com.itextpdf.text.Document
+import com.itextpdf.text.Paragraph
+import com.itextpdf.text.pdf.PdfWriter
 import kotlinx.android.synthetic.main.activity_iplookup.*
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
@@ -29,6 +32,8 @@ import retrofit2.converter.gson.GsonConverterFactory
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 import java.util.regex.Matcher
 import java.util.regex.Pattern
 
@@ -40,14 +45,30 @@ class iplookup : AppCompatActivity() {
                 + "[0-9]{2}|[1-9][0-9]|[1-9]|0)\\.(25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}"
                 + "|[1-9][0-9]|[0-9]))"
     )
-
+    var users = arrayOf<String>()
+    private val STORAGE_CODE: Int = 100
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
         button2.setOnClickListener() {
-            createPdf("hii hello")
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
+                //system OS >= Marshmallow(6.0), check permission is enabled or not
+                if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_DENIED
+                ) {
+                    //permission was not granted, request it
+                    val permissions = arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    requestPermissions(permissions, STORAGE_CODE)
+                } else {
+                    //permission already granted, call savePdf() method
+                    createPdf(users)
+                }
+            } else {
+                //system OS < marshmallow, call savePdf() method
+                createPdf(users)
+            }
         }
         button.setOnClickListener {
 
@@ -72,7 +93,7 @@ class iplookup : AppCompatActivity() {
                     override fun onResponse(call: Call<ipdata>, response: Response<ipdata>) {
                         Log.d("DATA", response.body().toString())
 
-                        val users = arrayOf(
+                        users = arrayOf(
                             "HOST= " + response.body()?.data?.geo?.host,
                             "IP= " + response.body()?.data?.geo?.ip,
                             "RDNS= " + response.body()?.data?.geo?.rdns,
@@ -160,25 +181,63 @@ class iplookup : AppCompatActivity() {
         document.close()
     }
 
-    @RequiresApi(Build.VERSION_CODES.M)
-    private fun setupPermissions() {
-        val readexternalpermission = ContextCompat.checkSelfPermission(
-            this,
-            Manifest.permission.READ_EXTERNAL_STORAGE
-        )
-        val writeexternalpermission = ContextCompat.checkSelfPermission(
-            this,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE
-        )
-
-        if (readexternalpermission != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(arrayOf<String>(Manifest.permission.READ_EXTERNAL_STORAGE),123)
-            Toast.makeText(applicationContext, "read Permission Denia", Toast.LENGTH_SHORT).show()
-        }
-        if (writeexternalpermission != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(arrayOf<String>(Manifest.permission.WRITE_EXTERNAL_STORAGE),123)
-            Toast.makeText(applicationContext, "write Permission Denia", Toast.LENGTH_SHORT).show()
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        //super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            STORAGE_CODE -> {
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //permission from popup was granted, call savePdf() method
+                    //createPdf("hii hello")
+                } else {
+                    //permission from popup was denied, show error message
+                    Toast.makeText(this, "Permission denied...!", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
+    private fun createPdf(sometext: Array<String>) { // create a new document
+        val mDoc = Document()
+        //pdf file name
+        val mFileName = SimpleDateFormat(
+            "yyyyMMdd_HHmmss",
+            Locale.getDefault()
+        ).format(System.currentTimeMillis())
+        //pdf file path
+        val mFilePath =
+            Environment.getExternalStorageDirectory().toString() + "/" + mFileName + ".pdf"
+        try {
+            //create instance of PdfWriter class
+            PdfWriter.getInstance(mDoc, FileOutputStream(mFilePath))
+
+            //open the document for writing
+            mDoc.open()
+
+            //get text from EditText i.e. textEt
+
+            //add author of the document (metadata)
+            mDoc.addAuthor("Atif Pervaiz")
+
+            //add paragraph to the document
+            //mDoc.add(Paragraph(sometext))
+            for (i in sometext) {
+                mDoc.add(Paragraph(i))
+            }
+            //close document
+            mDoc.close()
+
+            //show file saved message with file name and path
+            Toast.makeText(this, "$mFileName.pdf\nis saved to\n$mFilePath", Toast.LENGTH_SHORT)
+                .show()
+        } catch (e: Exception) {
+            //if anything goes wrong causing exception, get and show exception message
+            Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
 
 }
