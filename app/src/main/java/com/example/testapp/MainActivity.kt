ef701cf2c989e827b5537e5d6e8142aa07c293d1
rushilrushat <rushilrushat@gmail.com>
2020-03-21 20:52:16 +0530

    Tower Info Page add

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -2,21 +2,8 @@ package com.example.testapp
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.widget.ArrayAdapter
-import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
-import com.example.testapp.iplookup.data.ipdata
-import com.example.testapp.iplookup.Api
-import com.example.testapp.iplookup.iplookup
 import kotlinx.android.synthetic.main.activity_main.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import java.util.regex.Matcher
-import java.util.regex.Pattern
 
 
 class MainActivity : AppCompatActivity() {
@@ -26,12 +13,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        ip_button.setOnClickListener() {
+        ip_button.setOnClickListener {
             intent = Intent(this, com.example.testapp.iplookup.iplookup::class.java)
             startActivity(intent)
         }
-        true_button.setOnClickListener(){
+        true_button.setOnClickListener{
             intent = Intent(this, com.example.testapp.trucaller.trucaller::class.java)
+            startActivity(intent)
+        }
+        tower_info.setOnClickListener {
+            intent = Intent(this,com.example.testapp.Towerinfo::class.java)
             startActivity(intent)
         }
 
---
+++ app/src/main/java/com/example/testapp/Towerinfo.kt
@@ -0,0 +1,78 @@
+package com.example.testapp
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.telephony.CellLocation
+import android.telephony.TelephonyManager
+import android.telephony.gsm.GsmCellLocation
+import android.widget.TextView
+import androidx.core.app.ActivityCompat
+
+class Towerinfo : AppCompatActivity() {
+    var telephonyManager: TelephonyManager? = null
+    var lac = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_towerinfo)
+        val ty = findViewById<TextView>(R.id.tv)
+        val mnc = findViewById<TextView>(R.id.mnc)
+        val mcc = findViewById<TextView>(R.id.mcc)
+        val lac = findViewById<TextView>(R.id.loc)
+        val cid = findViewById<TextView>(R.id.cellid)
+        telephonyManager =
+            getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ),
+                100
+            )
+        }
+        val gsmCellLocation =
+            telephonyManager!!.cellLocation as GsmCellLocation
+        val gsmCellLocation2 =
+            gsmCellLocation(gsmCellLocation)
+        var i = -1
+        this.lac = gsmCellLocation2?.lac ?: -1
+        if (gsmCellLocation2 != null) {
+            i = gsmCellLocation2.cid
+        }
+        cid.text = "Cell-ID $i"
+        val networkOperator = telephonyManager!!.networkOperator
+        mnc.text = "MNC " + networkOperator.substring(3)
+        mcc.text = "MCC " + networkOperator.substring(0, 3)
+        ty.text = telephonyManager!!.networkOperatorName + "," + getNetworkType(telephonyManager)
+        lac.text = "Location " + this.lac
+    }
+    fun getNetworkType(telephonyManager2: TelephonyManager?): String {
+        return when (telephonyManager2!!.networkType) {
+            1, 2, 4, 7, 11 -> "2G"
+            3, 5, 6, 8, 9, 10, 12, 14, 15 -> "3G"
+            13 -> "4G"
+            else -> "Unknown"
+        }
+    }
+
+    companion object {
+        private fun gsmCellLocation(cellLocation: CellLocation): GsmCellLocation? {
+            return try {
+                cellLocation as GsmCellLocation
+            } catch (unused: ClassCastException) {
+                null
+            }
+        }
+    }
+}
